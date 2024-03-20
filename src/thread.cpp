1ecaa9bfdd04843d805381bf8e27e60894bed144
Ryan Mancera <mancerar@mcmaster.ca>
2024-03-20 18:10:35 -0400

    added thread functions

--- src/thread.cpp
+++ src/thread.cpp
@@ -1,3 +1,10 @@
 #include "dy4.h"
 #include "thread.h"
 
+void audio_thread() {
+
+}
+
+void rf_thread(const std::vector<float> &audio_data, std::vector<float> &I, std::vector<float> &Q, std::queue<int> &my_queue) {
+
+}
